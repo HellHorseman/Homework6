d0152855482823c0e6763f57035a09b657eebeab
HellHorseman <duke.sheffer@yandex.ru>
2023-02-20 20:10:06 +0400

    add task4

--- src/Main.java
+++ src/Main.java
@@ -16,24 +16,26 @@ public class Main {
         System.out.println("Задача 1");
     for (int i = 0; i <= 10; i++) {
         System.out.println(i);
-    }
+        }
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
     for (int i = 10; i >= 1; i--) {
         System.out.println(i);
-    }
+        }
     }
     public static void task3 () {
         System.out.println("Задача 3");
     for (int i = 0; i <= 17; i = i +2) {
         System.out.println(i);
-    }
+        }
     }
     public static void task4 () {
         System.out.println("Задача 4");
-
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
     public static void task5 () {
         System.out.println("Задача 5");
